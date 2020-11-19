f0df7a3147888ce561e43f959a29401ab0213b47
Maciej Karolczyk <Maciej.Karolczyk@gft.com>
2020-11-19 11:15:26 +0100

    Initial project setup

---
+++ MultiNetworking/MultiNetworking.h
@@ -0,0 +1,18 @@
+//
+//  MultiNetworking.h
+//  MultiNetworking
+//
+//  Created by Karolczyk, Maciej on 19/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiNetworking.
+FOUNDATION_EXPORT double MultiNetworkingVersionNumber;
+
+//! Project version string for MultiNetworking.
+FOUNDATION_EXPORT const unsigned char MultiNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiNetworking/PublicHeader.h>
+
+
