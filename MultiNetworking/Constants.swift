51ccac58e124ce25807c2e02a508e16b0e86aec3
Maciej Karolczyk <Maciej.Karolczyk@gft.com>
2020-11-19 11:45:39 +0100

    Added basic networking and test cases

---
+++ MultiNetworking/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MultiNetworking
+//
+//  Created by Karolczyk, Maciej on 19/11/2020.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let genericError = "Something went wrong. Please try again later"
+    static let noConnectionError = "Not connected to Internet"
+    
+    static let dailyMotionEndpoint = "https://api.dailymotion.com/users?fields=avatar_360_url,username"
+}
--- MultiNetworkingTests/MultiNetworkingTests.swift
+++ MultiNetworkingTests/MultiNetworkingTests.swift
@@ -10,24 +10,62 @@ import XCTest
 
 class MultiNetworkingTests: XCTestCase {
 
+    var sut: URLSession!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testValidCallToDailyMotionAPIGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: Constants.dailyMotionEndpoint)
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
         }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
     }
+    
+    func testCallToDailyMotionAPICompletes() {
+        // given
+        let url = URL(string: Constants.dailyMotionEndpoint)
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+
+    
 
 }
